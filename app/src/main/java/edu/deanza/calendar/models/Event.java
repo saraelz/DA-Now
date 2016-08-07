e474eac9b3bccecdd1ca9dfe66382b6103585775
Karina Antonio <karinafantonio@gmail.com>
2016-08-07 11:44:24 -0700

    Remove `tags` field and add TODO

--- app/src/main/java/edu/deanza/calendar/models/Event.java
+++ app/src/main/java/edu/deanza/calendar/models/Event.java
@@ -3,7 +3,6 @@ package edu.deanza.calendar.models;
 //constructor for localDateTime: LocalDateTime(int year, int monthOfYear, int dayOfMonth, int hourOfDay, int minuteOfHour)
 
 import org.joda.time.LocalTime;
-import java.util.ArrayList;
 
 /**
  * Created by Sara on 5/28/2016.
@@ -14,12 +13,7 @@ public class Event {
     protected enum source {
         Facebook, ClubMeeting, TransferAppointment, Website
     }
-    protected ArrayList<String> tags;
-
-    Event()
-    {
-
-    }
+    // TODO: add `categories` field
 
     Event(String name, String description, String location, String startTime,
           String endTime)
@@ -30,64 +24,6 @@ public class Event {
         this.startTime = LocalTime.parse(startTime);
         this.endTime = LocalTime.parse(endTime);
 
-        tags = new ArrayList<String>();
-        //generate_tags();
     }
 
-    //NEED TO ALPHABETIZE ALL TAGS
-    protected void generate_tags()
-    {
-        int tempDay = start.getDayOfWeek();
-        switch (tempDay)
-        {
-            case 1: tags.add("Mo");
-                break;
-            case 2: tags.add("Tu");
-                break;
-            case 3: tags.add("We");
-                break;
-            case 4: tags.add("Th");
-                break;
-            case 5: tags.add("Fr");
-                break;
-            case 6: tags.add("Sa");
-                break;
-            case 7: tags.add("Su");
-                break;
-            //no default case
-        }
-        //does this make sense to continue with month and month/day?
-        //cases from LocalDateTime API http://joda-time.sourceforge.net/apidocs/org/joda/time/DateTimeConstants.html
-
-        //This will split the string around the spaces and stores them into the given string array
-        String tempNameArr[] = name.split(" ");
-        for (String tempName : tempNameArr)
-        {
-            tags.add(tempName);
-        }
-
-        //excessive?
-        String tempDescriptionArr[] = description.split(" ");
-        for (String tempDescription : tempDescriptionArr)
-        {
-            tags.add(tempDescription);
-        }
-    }
-
-    //allows us to add any tag to list
-    public void tag(String t)
-    {
-        tags.add(t);
-        //sort/alphabetize new tag
-    }
-
-    /*public boolean search_tags(String input)
-    {
-        //return ArrayList<String> instead?
-        String keys[] = input.split(" ");
-        for (String key: keys)
-            if (tags.contains(key)) //or do another for loop, check beginning of input to match tags
-                return true;
-        //display in ListView
-    }*/
 }
