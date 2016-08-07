343fa295a489251dfd77c63f70f83851f6b87eee
Karina Antonio <karinafantonio@gmail.com>
2016-08-07 12:15:57 -0700

    Remove non-MVP Graph API calls

--- app/src/main/java/edu/deanza/calendar/SearchActivity.java
+++ app/src/main/java/edu/deanza/calendar/SearchActivity.java
@@ -3,13 +3,6 @@ package edu.deanza.calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.facebook.AccessToken;
-import com.facebook.FacebookSdk;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-import com.facebook.HttpMethod;
-import com.facebook.appevents.AppEventsLogger;
-
 import edu.deanza.R;
 
 public class SearchActivity extends AppCompatActivity {
@@ -19,36 +12,6 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        //track analytics using FB--can remove feature later
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
-
     }
 
-    private void FacebookStalking()
-    {
-        long [] facebookGroups = {885940821502890L, //De Anza DECA Club
-                219645201389350L, //VIDA: Vasconcellos Institute for Democracy In Action
-                2236277819L, //De Anza College
-        };
-        long [] facebookPages = {265047173570399L, //ICC
-                192472534143760L //DASB
-        };
-    }
-
-    private void grabFacebookPage(int id)
-    {
-        /* make the API call */
-        new GraphRequest(
-                AccessToken.getCurrentAccessToken(),
-                "/" + id + "/events",
-                null,
-                HttpMethod.GET,
-                new GraphRequest.Callback() {
-                    public void onCompleted(GraphResponse response) {
-                        /* handle the result */
-                    }
-                }
-        ).executeAsync();
-    }
 }
