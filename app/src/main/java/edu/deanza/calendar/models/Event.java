44b726d3cb1b576ff208723d08140c6d8b52917f
Karina Antonio <karinafantonio@gmail.com>
2016-08-11 03:37:51 -0700

    Remove JSON mapping code from domain models

--- app/src/main/java/edu/deanza/calendar/models/Event.java
+++ app/src/main/java/edu/deanza/calendar/models/Event.java
@@ -1,11 +1,6 @@
 package edu.deanza.calendar.models;
 
 import org.joda.time.DateTime;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 
 /**
  * Created by Sara on 5/28/2016.
@@ -32,70 +27,6 @@ public class Event {
         this.organization = organization;
         this.startTime = DateTime.parse(startTime);
         this.endTime = DateTime.parse(endTime);
-    }
-
-    //based on CalendarEvents.json - can handle only one object
-    public static Event fromCalendarEventsJSON(JSONObject jsonEvent) {
-        Event e = new Event();
-        try {
-            e.name = jsonEvent.getString("name");
-            e.description = jsonEvent.getString("description");
-            e.location = jsonEvent.getString("location");
-            e.startTime = DateTime.parse(jsonEvent.getString("start_time"));
-            e.endTime = DateTime.parse(jsonEvent.getString("end_time"));
-            e.organization = jsonEvent.getString("organization");
-        }
-        catch (JSONException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return e;
-    }
-
-    //based on CalendarEvents.json - can handle entire file
-    public static ArrayList<Event> fromCalendarEventsJSON(JSONArray jsonEvents) {
-        ArrayList<Event> events = new ArrayList<Event>(jsonEvents.length());
-        JSONObject jsonEvent;
-        for (int i = 0; i < jsonEvents.length(); i++) {
-            jsonEvent = jsonEvents.getJSONObject(i);
-            Event event = Event.fromCalendarEventsJSON(jsonEvent);
-
-            if (event != null) {
-                events.add(event);
-            }
-        }
-        return events;
-    }
-
-    //based on Clubs.json - can handle only one object
-    //pre: club information from a single JSONObject
-    //post: return all meeting times for a club
-    public static ArrayList<Event> fromClubsJSON(JSONObject jsonClub)
-    {
-        ArrayList<Event> clubMeetings = new ArrayList<Event>();
-        try {
-            String sponsor = jsonClub.getString("name");
-            String name = sponsor + " Club Meeting";
-            String description = jsonClub.getString("description");
-            String location = jsonClub.getString("location");
-            String start_time = jsonClub.getString("start_time");
-            String end_time = jsonClub.getString("end_time");
-            JSONArray clubDates = jsonClub.getJSONArray("dates");
-            for (int i = 0; i < clubDates.length(); i++)
-            {
-                String date = clubDates.getString(i);
-
-                start_time = date + "T" + start_time;
-                end_time = date + "T" + end_time;
-                Event e = new Event(name, sponsor, description, location, start_time, end_time);
-                clubMeetings.add(e);
-            }
-        }
-        catch (JSONException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return clubMeetings;
     }
 
     public String getName() {
--- app/src/main/java/edu/deanza/calendar/models/Organization.java
+++ app/src/main/java/edu/deanza/calendar/models/Organization.java
@@ -1,11 +1,5 @@
 package edu.deanza.calendar.models;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-
 /**
  * Created by soso1 on 8/8/2016.
  */
@@ -28,51 +22,6 @@ public class Organization {
         this.description = description;
         this.facebookUrl = facebookUrl;
     }
-
-    //based on Clubs.json
-    public static Organization fromClubsJSON(JSONObject jsonClub) {
-        Organization club = new Organization();
-        try {
-            club.name = jsonClub.getString("name");
-            club.description = jsonClub.getString("description");
-            club.facebookUrl = null;
-        }
-        catch (JSONException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return club;
-    }
-
-    //based on Clubs.json - can handle entire file
-    public static ArrayList<Organization> fromClubsJSON(JSONArray jsonClubs) {
-        ArrayList<Organization> clubs = new ArrayList<Organization>(jsonClubs.length());
-        JSONObject jsonClub;
-        for (int i = 0; i < jsonClubs.length(); i++) {
-            jsonClub = jsonClubs.getJSONObject(i);
-            Organization club = Organization.fromClubsJSON(jsonClub);
-
-            if (club != null) {
-                clubs.add(club);
-            }
-        }
-        return clubs;
-    }
-
-    /*//incomplete method, uses Facebook API
-    public static Organization fromFacebookURL(String url) {
-        Organization organization = new Organization();
-
-        //pull json from facebook
-
-        //generate name, description from JSON
-
-        //initialize organization.name, organization.description, organization.facebookUrl
-
-        //is there a need to treat pages different than groups?
-
-        return organization;
-    }*/
 
     public String getName() {
         return name;
